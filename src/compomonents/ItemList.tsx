8204c55e1497f1af47f49dfdcfb12a712a70f9b4
Alkan <alkanorgun@gmail.com>
2021-06-09 21:19:57 +0300

    ItemList.tsx

--- src/compomonents/ItemList.tsx
+++ src/compomonents/ItemList.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { FC } from "react";
+import Item from "./Item";
 
-function ItemList() {
-  return <div></div>;
+interface Props {
+  items: {
+    id: number;
+    title: string;
+    price: number;
+    img1: string;
+    img2: string;
+    img3: string;
+    img4: string;
+    amount: number;
+  }[];
+  addToCart: (id: number) => void;
+  changeAmountOfItemInList: (id: number, op: string) => void;
 }
+
+const ItemList: FC<Props> = ({
+  items,
+  addToCart,
+  changeAmountOfItemInList,
+}) => {
+  return (
+    <ul className="items-list">
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <Item
+              {...item}
+              addToCart={addToCart}
+              changeAmountOfItemInList={changeAmountOfItemInList}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ItemList;
