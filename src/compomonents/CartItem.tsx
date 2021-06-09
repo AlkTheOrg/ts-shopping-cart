285a8a2cefd7071e90a14386f63b9f5fc12b83ef
Alkan <alkanorgun@gmail.com>
2021-06-09 23:41:25 +0300

    CartItem.tsx

--- src/compomonents/CartItem.tsx
+++ src/compomonents/CartItem.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { FC } from "react";
 
-function CartItem() {
-  return <div></div>;
+interface Props {
+  id: number;
+  title: string;
+  price: number;
+  img1: string;
+  amount: number;
+  subtractFromCard: (id: number) => void;
+  increaseInCard: (id: number) => void;
 }
+
+const CartItem: FC<Props> = ({
+  id,
+  title,
+  price,
+  img1,
+  amount,
+  subtractFromCard,
+  increaseInCard,
+}): JSX.Element => {
+  return (
+    <div className="cart-item">
+      <h3>{title}</h3>
+      <div className="cart-item-body">
+        <img src={img1} alt="cart-item" />
+        <div className="cart-item-hero">
+          <div className="cart-item-amount">
+            <button
+              onClick={() => subtractFromCard(id)}
+              className="dec-item-amount"
+            >
+              -
+            </button>
+            <p>{amount}</p>
+            <button
+              onClick={() => increaseInCard(id)}
+              className="inc-item-amount"
+            >
+              +
+            </button>
+          </div>
+          <h4>{price * amount} TL</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CartItem;
