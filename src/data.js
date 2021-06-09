3db9bc3994e533ef6bd67ed10acef938471a38e6
Alkan <alkanorgun@gmail.com>
2021-06-09 23:52:52 +0300

    data and practice

---
+++ src/data.js
@@ -0,0 +1,144 @@
+export default [
+  {
+    id: 1,
+    title: `Asus X515JA-BR070T Intel Core i3-1005G 256 SSD 15.6" HD Win10 Notebook`,
+    price: 5189,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125036008/125036008_0_MC/50216576.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125036008/125036008_1_MC/50216577.jpg",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125036008/125036008_3_MC/47591469.png",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125036008/125036008_5_MC/47591470.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: `
+    Asus Laptop X515MA BR091T Celeron N4020 4GB RAM 128gb Ssd 15.6" W10 Notebook
+    `,
+    price: 3099,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038185/125038185_1_MC/51440501.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038185/125038185_2_MC/51440500.jpg",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038185/125038185_4_MC/51440512.jpg",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038185/125038185_9_MC/51440498.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: `
+    Acer A315-23 AMD Athlon 3050 4GB RAM 256GB SSD 15.6" W10 Notebook
+    `,
+    price: 3499,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037350/125037350_0_MC/49763592.png",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037350/125037350_1_MC/49763594.png",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037350/125037350_3_MC/49763600.png",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037350/125037350_4_MC/49763601.png",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: `
+    Casper Nirvana X400.1035-8U00T-S-F i5-1035G1 8GB RAM 240 GB SSD 14" Notebook
+    `,
+    price: 6049,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037137/125037137_0_MC/53091758.png",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037137/125037137_1_MC/53091759.png",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037137/125037137_2_MC/53091760.png",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037137/125037137_3_MC/53091761.png",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: `
+    Asus D515DA-BR027T Amd Ryzen5-3500u 8GB Ram 256GB SSD 15.6" W10 Notebook
+    `,
+    price: 5299,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038180/125038180_0_MC/51054343.png",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038180/125038180_1_MC/51054344.png",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038180/125038180_2_MC/51054345.png",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125038180/125038180_3_MC/51054346.png",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: `
+    Casper Nirvana X500.1005-4W00E-G-F i3-1005G1 4GB RAM 120GB SSD 15,6" Notebook
+    `,
+    price: 4899,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037136/125037136_0_MC/51441714.png",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125037136/125037136_1_MC/51441718.png",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: `
+    Preo My Sound MS15 Kulak Üstü Kablosuz Bluetooth Kulaklık Siyah
+    `,
+    price: 99.9,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262692/110262692_0_MC/28356744.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262692/110262692_2_MC/28356746.jpg",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: `
+    Preo My Sound MS23 Kulak İçi Kablolu 3.5mm Kulaklık Beyaz
+    `,
+    price: 555,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059565/145059565_0_MC/38532940.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059565/145059565_1_MC/38532941.jpg",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+  {
+    id: 9,
+    title: `
+    Preo My Sound Ms11 Kulak İçi Kulaklık Beyaz
+    `,
+    price: 234,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262580/110262580_0_MC/28354288.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262580/110262580_1_MC/28354291.png",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+  {
+    id: 10,
+    title: `
+    Preo My Sound MS23 Kulak İçi Kablolu 3.5mm Kulaklık Siyah
+    `,
+    price: 122,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059566/145059566_0_MC/38686323.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059566/145059566_1_MC/38686324.jpg",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+  {
+    id: 11,
+    title: `
+    Preo My Sound MS15 Kulak Üstü Kablosuz Bluetooth Kulaklık Kırmızı
+    `,
+    price: 145,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262693/110262693_0_MC/28356697.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262693/110262693_1_MC/28356699.jpg",
+    img3: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262693/110262693_3_MC/28356706.jpg",
+    img4: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262693/110262693_4_MC/28356725.jpg",
+    amount: 1,
+  },
+  {
+    id: 12,
+    title: `
+    Preo My Sound Ms11 Kulak İçi Kulaklık Siyah
+    `,
+    price: 37,
+    img1: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262578/110262578_0_MC/28354343.jpg",
+    img2: "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110262578/110262578_1_MC/28354348.png",
+    img3: "",
+    img4: "",
+    amount: 1,
+  },
+];
