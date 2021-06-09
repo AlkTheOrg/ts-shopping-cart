fe68c581e1b9e241e6b66ca80f6e26fca3da5a34
Alkan <alkanorgun@gmail.com>
2021-06-09 23:41:14 +0300

    Cart.tsx

--- src/compomonents/Cart.tsx
+++ src/compomonents/Cart.tsx
@@ -1,6 +1,13 @@
 import React, { FC } from "react";
+import { ItemData } from "../data";
+import CartItem from "./CartItem";
 
-interface Props {}
+interface Props {
+  cart: ItemData[];
+  totalCost: number;
+  subtractFromCard: (id: number) => void;
+  increaseInCard: (id: number) => void;
+}
 
 // function Cart2(): JSX.Element {
 //   return <div></div>;
@@ -9,8 +16,28 @@ interface Props {}
 /* Without FC<Props> Cart is an anynmous function
 that returns JSX.Element. Now it s an FC<Props> element.
 */
-const Cart: FC<Props> = (): JSX.Element => {
-  return <div></div>;
+const Cart: FC<Props> = ({
+  cart,
+  totalCost,
+  subtractFromCard,
+  increaseInCard,
+}): JSX.Element => {
+  return (
+    <aside className="Cart">
+      <h1>Cart</h1>
+      {cart.map((item: ItemData) => {
+        return (
+          <CartItem
+            {...item}
+            subtractFromCard={subtractFromCard}
+            increaseInCard={increaseInCard}
+          />
+        );
+      })}
+      <h2 className="total-cost">{totalCost} TL</h2>
+      <button className="proceed">PROCEED</button>
+    </aside>
+  );
 };
 
 export default Cart;
