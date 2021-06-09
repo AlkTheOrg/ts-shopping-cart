1050d3199e79d23d296b28a9e39cd3bf5f252b11
Alkan <alkanorgun@gmail.com>
2021-06-09 21:12:27 +0300

    Item.tsx

--- src/compomonents/Item.tsx
+++ src/compomonents/Item.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { FC } from "react";
 
-function Item() {
-  return <div></div>;
+interface Props {
+  id: number;
+  title: string;
+  price: number;
+  img1: string;
+  amount: number;
+  addToCart: (id: number) => void;
+  changeAmountOfItemInList: (id: number, op: string) => void;
 }
+
+const Item: FC<Props> = ({
+  id,
+  title,
+  price,
+  img1,
+  amount,
+  addToCart,
+  changeAmountOfItemInList,
+}): JSX.Element => {
+  return (
+    <div className="item-wrapper">
+      <div className="item">
+        <a href={`/item/${id}`}>
+          <h1>{title}</h1>
+          <img src={img1} alt="item" className="item-img" />
+        </a>
+        <h2>{price}</h2>
+        <div className="amount-wrapper">
+          <div className="amount">
+            <div className="btn-container">
+              <button
+                onClick={() => changeAmountOfItemInList(id, "DEC")}
+                className="decrement"
+              >
+                -
+              </button>
+            </div>
+            <div className="btn-container">
+              <button
+                onClick={() => changeAmountOfItemInList(id, "INC")}
+                className="increment"
+              >
+                +
+              </button>
+            </div>
+            <div className="btn-container">
+              <button onClick={() => addToCart(id)} className="add-btn">
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Item;
