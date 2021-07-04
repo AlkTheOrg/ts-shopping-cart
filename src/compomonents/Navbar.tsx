1c70ea6c5e929b60a9c0850c8e1319765e8a2051
Alkan <alkanorgun@gmail.com>
2021-07-04 19:39:15 +0300

    Navbar

--- src/compomonents/Navbar.tsx
+++ src/compomonents/Navbar.tsx
@@ -3,11 +3,27 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <Link to="/">
-      <div>
-        <h1>THIS IS A NAVBAR</h1>
-      </div>
-    </Link>
+    <nav className="main-nav">
+      <ul className="nav-list">
+        <div className="react-nav-links">
+          <li className="react-nav-link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="react-nav-link">
+            <Link to="/about">About</Link>
+          </li>
+        </div>
+        <li className="nav-list-elem">
+          <a id="github" href="github.com/AlkTheOrg">
+            <img
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt=""
+              style={{ width: "44px", mixBlendMode: "multiply" }}
+            />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
